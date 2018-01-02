1b50ddf5bd0fd11c78b229c5a6cf751dd5b7f10a
chenqiushi <tracy909@126.com>
2018-01-02 15:56:47 +0800

    update

--- lavas-demo/appshell-new/service-worker.js
+++ lavas-demo/appshell-new/service-worker.js
@@ -9,7 +9,7 @@
  * 1. Need adding publicPath manually.
  * 2. If the version of workbox updated, modification is also required.
  */
-importScripts('/static/js/workbox-sw.prod.v2.1.2.js');
+importScripts('/lavas-demo/appshell-new/static/js/workbox-sw.prod.v2.1.2.js');
 
 const workboxSW = new WorkboxSW({
     cacheId: 'lavas-cache',
@@ -21,39 +21,45 @@ const workboxSW = new WorkboxSW({
 // Define precache injection point.
 workboxSW.precache([
   {
-    "url": "/lavas-demo/index.html",
-    "revision": "220d6ba3304626f9fb98d11f5018c1b2"
+    "url": "/lavas-demo/appshell-new/index.html",
+    "revision": "27f3926cff3c8ae8040484e78de13cc3"
   },
   {
-    "url": "/lavas-demo/static/css/index.ed5c7c4c.css"
+    "url": "/lavas-demo/appshell-new/static/css/index.ed5c7c4c.css"
   },
   {
-    "url": "/lavas-demo/static/fonts/MaterialIcons-Regular.ttf",
+    "url": "/lavas-demo/appshell-new/static/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "url": "/lavas-demo/appshell-new/static/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "url": "/lavas-demo/appshell-new/static/fonts/MaterialIcons-Regular.ttf",
     "revision": "a37b0c01c0baf1888ca812cc0508f6e2"
   },
   {
-    "url": "/lavas-demo/static/fonts/MaterialIcons-Regular.woff",
+    "url": "/lavas-demo/appshell-new/static/fonts/MaterialIcons-Regular.woff",
     "revision": "012cf6a10129e2275d79d6adac7f3b02"
   },
   {
-    "url": "/lavas-demo/static/js/index.26e312c6.js"
+    "url": "/lavas-demo/appshell-new/static/js/index.009ba3ac.js"
   },
   {
-    "url": "/lavas-demo/static/js/manifest.17a0d9e2.js"
+    "url": "/lavas-demo/appshell-new/static/js/manifest.cfcd855d.js"
   },
   {
-    "url": "/lavas-demo/static/js/vendor.db046167.js"
+    "url": "/lavas-demo/appshell-new/static/js/vendor.ded9ba07.js"
   },
   {
-    "url": "/lavas-demo/static/js/vue.4dca8e7e.js"
+    "url": "/lavas-demo/appshell-new/static/js/vue.be7f9f65.js"
   },
   {
-    "url": "/lavas-demo/static/js/workbox-sw.prod.v2.1.2.js",
+    "url": "/lavas-demo/appshell-new/static/js/workbox-sw.prod.v2.1.2.js",
     "revision": "685d1ceb6b9a9f94aacf71d6aeef8b51"
   },
   {
     "url": "/appshell",
-    "revision": "513c98ce0d8cfeb95f3c93f4d96da424"
+    "revision": "a3a7382415886137f13607009bce534a"
   }
 ]);
 
