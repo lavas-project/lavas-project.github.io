ab08e23e3567f06922dc95f9d541d190cc56daae
chenqiushi <tracy909@126.com>
2018-01-11 19:11:42 +0800

    调整 polyfill 顺序

--- lavas-demo/appshell-new/service-worker.js
+++ lavas-demo/appshell-new/service-worker.js
@@ -22,7 +22,7 @@ const workboxSW = new WorkboxSW({
 workboxSW.precache([
   {
     "url": "/lavas-demo/appshell-new/index.html",
-    "revision": "a2202fc2d968e8dd5f9f738c29a39a55"
+    "revision": "a2bb78d4b344c9e4c0652d92f4a5351b"
   },
   {
     "url": "/lavas-demo/appshell-new/static/css/index.15045866.css"
@@ -42,16 +42,16 @@ workboxSW.precache([
     "revision": "012cf6a10129e2275d79d6adac7f3b02"
   },
   {
-    "url": "/lavas-demo/appshell-new/static/js/index.c6edbca9.js"
+    "url": "/lavas-demo/appshell-new/static/js/index.c4d2ab98.js"
   },
   {
-    "url": "/lavas-demo/appshell-new/static/js/manifest.0e06255c.js"
+    "url": "/lavas-demo/appshell-new/static/js/manifest.a884ab14.js"
   },
   {
-    "url": "/lavas-demo/appshell-new/static/js/vendor.b86cb431.js"
+    "url": "/lavas-demo/appshell-new/static/js/vendor.b2ce233b.js"
   },
   {
-    "url": "/lavas-demo/appshell-new/static/js/vue.002aa181.js"
+    "url": "/lavas-demo/appshell-new/static/js/vue.43a05400.js"
   },
   {
     "url": "/lavas-demo/appshell-new/static/js/workbox-sw.prod.v2.1.2.js",
